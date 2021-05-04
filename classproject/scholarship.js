78f1ccd78121da89a9ad9e5d009479482aee5d24
thaopham98 <79811818+thaopham98@users.noreply.github.com>
2021-05-03 23:51:13 -0500

    classproject

---
+++ classproject/scholarship.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#t1").click(function(){
+        $("#schlarships").slideToggle("slow");
+        })
+    
+    $("#t2").click(function(){
+        $("#types").slideToggle("slow");
+        })
+    
+    $("#t3").click(function(){
+        $("#recent").slideToggle("slow");
+        })
+    
+    $("#t4").click(function(){
+        $("#recipients").slideToggle("slow");
+    });
+});
